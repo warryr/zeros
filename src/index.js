3cba961f3f06e85671652b9a8631f743ec59c8c2
warryr <heartsyrop@gmail.com>
2018-09-27 23:04:56 +0300

    Task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let zeros = Math.min(countTwos(number), countFives(number));
+
+  function countTwos(number){
+    let t = 0;
+    while(number > 1){
+      number = Math.floor(number/2);
+      t += number;
+    }
+    return t;
+  }
+  
+  function countFives(number){
+    let f = 0;
+    while(number > 4){
+      number = Math.floor(number/5);
+      f += number;
+    }
+    return f;
+  }
+  return zeros;
 }
